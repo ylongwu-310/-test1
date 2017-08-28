9f2f8cd6c6294e96ccf54d6b39167c3b5ee7442a
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2017-08-28 19:41:17 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,36 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
-urls config
-"""
-from django.conf.urls import patterns, include, url
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-# admin.autodiscover()
-from django.conf import settings
-# 公共URL配置
-urlpatterns = patterns(
-    '',
-    # Django后台数据库管理
-    url(r'^admin/', include(admin.site.urls)),
-    # 用户登录鉴权--请勿修改
-    url(r'^account/', include('account.urls')),
-    # 应用功能开关控制--请勿修改
-    url(r'^app_control/', include('app_control.urls')),
-    # 在home_application(根应用)里开始开发你的应用的主要功能
-    #url(r'^', include('home_application.urls')),
-    url(r'^', 'home_application.views.index'),
-    
-    # 组件样例
-    url(r'^test_component/', include('test_component.urls')),
-    # 后台任务样例
-    url(r'^test_celery/', include('test_celery.urls')),
-    # 通用Tags样例
-    url(r'^test_app_tags/', include('test_app_tags.urls')),
-    #url(r'^', include('home_application.urls')),
-)
+
+from common.mymako import render_mako_context
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello Word")
 
 
-handler404 = 'error_pages.views.error_404'
-handler500 = 'error_pages.views.error_500'
-handler403 = 'error_pages.views.error_403'
-handler401 = 'error_pages.views.error_401'
+def home(request):
+    """
+    首页
+    """
+    return render_mako_context(request, '/home_application/home.html')
+
+
+def dev_guide(request):
+    """
+    开发指引
+    """
+    return render_mako_context(request, '/home_application/dev_guide.html')
+
+
+def contactus(request):
+    """
+    联系我们
+    """
+    return render_mako_context(request, '/home_application/contact.html')
