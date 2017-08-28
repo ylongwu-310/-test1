c8bbb2fba07e516b1892c401138166c35a2adc87
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2017-08-28 19:44:17 +0800

    Update settings_testing.py

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -2,8 +2,7 @@
 """
 用于测试环境的全局配置
 """
-import os
-from settings import BASE_DIR
+from settings import APP_ID
 
 
 # ===============================================================================
@@ -12,6 +11,10 @@ from settings import BASE_DIR
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                      # 数据库名 (默认与APP_ID相同)
-    }
+        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'USER': '',                            # 你的数据库user
+        'PASSWORD': '',                        # 你的数据库password
+        'HOST': '',                   		   # 数据库HOST
+        'PORT': '3306',                        # 默认3306
+    },
 }
