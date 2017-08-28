bc2251ffe4f4a5f409d9f1f4ec70a3f4d8ccd09d
ylongwu-310 <31396909+ylongwu-310@users.noreply.github.com>
2017-08-28 11:45:05 +0800

    Update settings_testing.py

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -11,10 +11,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
+        'NAME': bktest,                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                            # 你的数据库user
         'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'HOST': 'localhost',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
